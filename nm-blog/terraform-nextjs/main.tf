db361ea28d7be7f38f09a05787705d6f5689fabb
Nilaj0914 <nilaj.majumder0814@gmail.com>
2025-06-02 20:53:25 +0530

    updated main.tf

--- nm-blog/terraform-nextjs/main.tf
+++ nm-blog/terraform-nextjs/main.tf
@@ -67,3 +67,55 @@ resource "aws_s3_bucket_policy" "website_policy" {
   }))
 }
 
+# Origin Access Identity
+resource "aws_cloudfront_origin_access_identity" "Website_OAI" {
+  comment = "OAI for NextJS portfolio site"
+}
+
+#Cloudfront Distribution
+resource "aws_cloudfront_distribution" "website_distribution" {
+  
+  origin {
+    domain_name = aws_s3_bucket.website.bucket_regional_domain_name
+    origin_id = "S3-website-bucket"
+
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.Website_OAI.cloudfront_access_identity_path
+    }
+  }
+
+  enabled = true
+  is_ipv6_enabled = true
+  comment = "Next.js portfolio site"
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    allowed_methods = ["GET", "HEAD", "OPTIONS"]
+    cached_methods = [ "GET", "HEAD" ]
+    target_origin_id = "S3-Website"
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl = 0
+    default_ttl = 3600
+    max_ttl = 86400
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = none
+    }
+  }
+
+  viewer_certificate {
+    
+    cloudfront_default_certificate = true
+    
+  }
+}
